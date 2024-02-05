9df01fccc0cb32520c519c2100b8358d5af2eb4e
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-02-05 20:50:13 +0500

    The commit introduces the use of React's `useReducer` and `useRef` hooks. - It adds a simple counter example using `useReducer` with actions to increment, decrement, and reset a count state. - It adds an example of `useRef` to focus and write a value to an input element.

--- src/App.js
+++ src/App.js
@@ -442,12 +442,51 @@
 //   );
 // }
 
-function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
-}
+// useReduce of React Example
+// import { useReducer } from "react";
+// function App() {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   return (
+//     <>
+//       <h1>{state.count}</h1>
+//       <button onClick={() => dispatch({ type: "increment" })}>➕</button>
+//       <button onClick={() => dispatch({ type: "decrement" })}>➖</button>
+//       <button onClick={() => dispatch({ type: "reset" })}>💣</button>
+//     </>
+//   );
+// }
+
+// const initialState = { count: 0 };
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return { ...state, count: state.count + 1 };
+//     case "decrement":
+//       return { ...state, count: state.count - 1 };
+//     case "reset":
+//       return { ...state, count: 0 };
+//     default:
+//       return state;
+//   }
+// };
+
+// useRef of React example {It is use to access the properties of the DOM element}
+// import { useRef } from "react";
+// function App() {
+//   let inputEle = useRef(null);
+
+//   let write = () => {
+//     inputEle.current.focus();
+//     inputEle.current.value = "Momin Ishtiaq";
+//   };
+
+//   return (
+//     <>
+//       <input type="text" ref={inputEle} />
+//       <button onClick={write}>Focus and Write Momin</button>
+//     </>
+//   );
+// }
 
 export default App;
